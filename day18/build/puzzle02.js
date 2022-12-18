6d58bbd806058b6d111be7fff6c67bf88459d24d
Kay Huber <kay.huber@unblu.com>
2022-12-18 16:18:25 +0100

    Day 18 puzzles - part 2 done

--- day18/build/puzzle02.js
+++ day18/build/puzzle02.js
@@ -11,6 +11,9 @@ class Vector {
         this.y = y;
         this.z = z;
     }
+    toString() {
+        return `{${this.x},${this.y},${this.z}}`;
+    }
 }
 let minX = Number.POSITIVE_INFINITY;
 let minY = Number.POSITIVE_INFINITY;
@@ -21,18 +24,6 @@ let maxZ = Number.NEGATIVE_INFINITY;
 let zLength = 0;
 let yLength = 0;
 let xLength = 0;
-const isOccupied = (space, vector) => {
-    if (vector.x - minX < 0 || vector.x - minX >= xLength) {
-        return false;
-    }
-    if (vector.y - minY < 0 || vector.y - minY >= yLength) {
-        return false;
-    }
-    if (vector.z - minZ < 0 || vector.z - minZ >= zLength) {
-        return false;
-    }
-    return space[vector.z - minZ][vector.y - minY][vector.x - minX];
-};
 const cubes = [];
 line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
     if (line.length > 0) {
@@ -60,102 +51,86 @@ line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
             }
         }
         cubes.forEach(cube => space[cube.z - minZ][cube.y - minY][cube.x - minX] = true);
-        // part about puzzle2: Find "inside holes". We do: check each non-occupied piece in space
-        // and see whether going in all directions it ever hits another cube and it does so in all directions
-        // we know it's an empty space and pretend it being a cube (i.e. we "fill" the hole)
-        const voidSpaces = [];
-        for (let z = 0; z < space.length; z++) {
-            for (let y = 0; y < space[z].length; y++) {
-                for (let x = 0; x < space[z][y].length; x++) {
-                    if (!space[z][y][x]) {
-                        voidSpaces.push(new Vector(x, y, z));
+        // part about puzzle2: Find "inside holes".
+        // We do:
+        // * invert the space to turn material into void and void into material
+        // * for each cube find all connected cubes and mark it in the inverse
+        // * in the inverse space, transfer all holes to the normal space and make them not count
+        // The cubes that cannot be reached are holes. We will fill them in the last step
+        // and then check the surface
+        const inverseSpace = new Array(zLength + 2);
+        for (let z = 0; z < zLength + 2; z++) {
+            inverseSpace[z] = new Array(yLength + 2);
+            for (let y = 0; y < yLength + 2; y++) {
+                inverseSpace[z][y] = new Array(xLength + 2);
+                for (let x = 0; x < xLength + 2; x++) {
+                    if (z === 0 || z === zLength + 1 || y === 0 || y === yLength + 1 || x === 0 || x === xLength + 1) {
+                        inverseSpace[z][y][x] = true;
+                    }
+                    else {
+                        inverseSpace[z][y][x] = !space[z - 1][y - 1][x - 1];
                     }
                 }
             }
         }
-        const insideHoles = voidSpaces.filter(vector => {
-            let foundObstacle = false;
-            // Z up down
-            for (let z = vector.z + 1; z < zLength; z++) {
-                if (space[z][vector.y][vector.x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+        const isOccupiedInInverseSpace = (vector) => {
+            if (vector.x < 0 || vector.x > xLength + 1) {
                 return false;
             }
-            foundObstacle = false;
-            for (let z = vector.z - 1; z >= 0; z--) {
-                if (space[z][vector.y][vector.x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+            if (vector.y < 0 || vector.y > yLength + 1) {
                 return false;
             }
-            // Y up down
-            foundObstacle = false;
-            for (let y = vector.y + 1; y < yLength; y++) {
-                if (space[vector.z][y][vector.x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+            if (vector.z < 0 || vector.z > zLength + 1) {
                 return false;
             }
-            foundObstacle = false;
-            for (let y = vector.y - 1; y >= 0; y--) {
-                if (space[vector.z][y][vector.x]) {
-                    foundObstacle = true;
-                    break;
+            return inverseSpace[vector.z][vector.y][vector.x];
+        };
+        const adjacentVectors = [
+            { x: 0, y: 0, z: 1 },
+            { x: 0, y: 0, z: -1 },
+            { x: 0, y: 1, z: 0 },
+            { x: 0, y: -1, z: 0 },
+            { x: 1, y: 0, z: 0 },
+            { x: -1, y: 0, z: 0 }
+        ];
+        const rootCube = new Vector(0, 0, 0);
+        let discoveredCubes = [];
+        let suspect = rootCube;
+        while (suspect !== undefined) {
+            const newSuspects = adjacentVectors
+                // @ts-ignore
+                .map(direction => new Vector(suspect.x + direction.x, suspect.y + direction.y, suspect.z + direction.z))
+                .filter(target => isOccupiedInInverseSpace(target));
+            discoveredCubes = discoveredCubes.concat(newSuspects);
+            inverseSpace[suspect.z][suspect.y][suspect.x] = false;
+            suspect = discoveredCubes.pop();
+        }
+        for (let z = 1; z < zLength + 1; z++) {
+            for (let y = 1; y < yLength + 1; y++) {
+                for (let x = 1; x < xLength + 1; x++) {
+                    if (inverseSpace[z][y][x]) {
+                        space[z - 1][y - 1][x - 1] = true;
+                    }
                 }
             }
-            if (!foundObstacle) {
+        }
+        const isOccupiedInNormalSpace = (vector) => {
+            if (vector.x - minX < 0 || vector.x - minX >= xLength) {
                 return false;
             }
-            foundObstacle = false;
-            for (let x = vector.x + 1; x < xLength; x++) {
-                if (space[vector.z][vector.y][x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+            if (vector.y - minY < 0 || vector.y - minY >= yLength) {
                 return false;
             }
-            foundObstacle = false;
-            for (let x = vector.x - 1; x >= 0; x--) {
-                if (space[vector.z][vector.y][x]) {
-                    foundObstacle = true;
-                    break;
-                }
+            if (vector.z - minZ < 0 || vector.z - minZ >= zLength) {
+                return false;
             }
-            return foundObstacle;
-        });
-        insideHoles.forEach(holeVector => space[holeVector.z][holeVector.y][holeVector.x] = true);
+            return space[vector.z - minZ][vector.y - minY][vector.x - minX];
+        };
         let exposedSurfaces = 0;
-        cubes.forEach(cube => {
-            if (!isOccupied(space, new Vector(cube.x, cube.y, cube.z + 1))) {
-                exposedSurfaces++;
-            }
-            if (!isOccupied(space, new Vector(cube.x, cube.y, cube.z - 1))) {
-                exposedSurfaces++;
-            }
-            if (!isOccupied(space, new Vector(cube.x, cube.y + 1, cube.z))) {
-                exposedSurfaces++;
-            }
-            if (!isOccupied(space, new Vector(cube.x, cube.y - 1, cube.z))) {
-                exposedSurfaces++;
-            }
-            if (!isOccupied(space, new Vector(cube.x + 1, cube.y, cube.z))) {
-                exposedSurfaces++;
-            }
-            if (!isOccupied(space, new Vector(cube.x - 1, cube.y, cube.z))) {
-                exposedSurfaces++;
-            }
+        cubes
+            .filter(cube => space[cube.z - minZ][cube.y - minY][cube.x - minX])
+            .forEach(cube => {
+            exposedSurfaces += adjacentVectors.filter(vector => !isOccupiedInNormalSpace(new Vector(cube.x + vector.x, cube.y + vector.y, cube.z + vector.z))).length;
         });
         console.log("Total surface area: ", exposedSurfaces);
     }
--- day18/src/puzzle02.ts
+++ day18/src/puzzle02.ts
@@ -12,6 +12,10 @@ class Vector {
         this.y = y;
         this.z = z;
     }
+
+    toString(): string {
+        return `{${this.x},${this.y},${this.z}}`;
+    }
 }
 
 let minX: number = Number.POSITIVE_INFINITY;
@@ -24,22 +28,6 @@ let maxZ: number = Number.NEGATIVE_INFINITY;
 let zLength: number = 0;
 let yLength: number = 0;
 let xLength: number = 0;
-
-const isOccupied = (space: boolean[][][], vector: Vector) => {
-    if (vector.x - minX < 0 || vector.x - minX >= xLength) {
-        return false;
-    }
-
-    if (vector.y - minY < 0 || vector.y - minY >= yLength) {
-        return false;
-    }
-
-    if (vector.z - minZ < 0 || vector.z - minZ >= zLength) {
-        return false;
-    }
-
-    return space[vector.z - minZ][vector.y - minY][vector.x - minX];
-}
 
 const cubes: Vector[] = [];
 linereader.eachLine("./input/input.txt", (line, last) => {
@@ -73,118 +61,98 @@ linereader.eachLine("./input/input.txt", (line, last) => {
 
         cubes.forEach(cube => space[cube.z - minZ][cube.y - minY][cube.x - minX] = true);
 
-        // part about puzzle2: Find "inside holes". We do: check each non-occupied piece in space
-        // and see whether going in all directions it ever hits another cube and it does so in all directions
-        // we know it's an empty space and pretend it being a cube (i.e. we "fill" the hole)
-        const voidSpaces: Vector[] = [];
-        for (let z=0; z < space.length; z++) {
-            for (let y=0; y < space[z].length; y++) {
-                for (let x=0; x < space[z][y].length; x++) {
-                    if (!space[z][y][x]) {
-                        voidSpaces.push(new Vector(x, y, z));
+        // part about puzzle2: Find "inside holes".
+        // We do:
+        // * invert the space to turn material into void and void into material
+        // * for each cube find all connected cubes and mark it in the inverse
+        // * in the inverse space, transfer all holes to the normal space and make them not count
+        // The cubes that cannot be reached are holes. We will fill them in the last step
+        // and then check the surface
+
+        const inverseSpace: boolean[][][] = new Array(zLength + 2);
+        for (let z=0; z < zLength + 2; z++) {
+            inverseSpace[z] = new Array(yLength + 2);
+            for (let y=0; y < yLength + 2; y++) {
+                inverseSpace[z][y] = new Array(xLength + 2);
+                for (let x=0; x < xLength + 2; x++) {
+                    if (z === 0 || z === zLength + 1 || y === 0 || y === yLength + 1 || x === 0 || x === xLength + 1) {
+                        inverseSpace[z][y][x] = true;
+                    } else {
+                        inverseSpace[z][y][x] = !space[z - 1][y - 1][x - 1];
                     }
                 }
             }
         }
 
-        const insideHoles: Vector[] = voidSpaces.filter(vector => {
-            let foundObstacle: boolean = false;
-
-            // Z up down
-            for (let z=vector.z + 1; z < zLength; z++) {
-                if (space[z][vector.y][vector.x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+        const isOccupiedInInverseSpace = (vector: Vector) => {
+            if (vector.x < 0 || vector.x > xLength + 1) {
                 return false;
             }
 
-            foundObstacle = false;
-            for (let z=vector.z - 1; z >= 0; z--) {
-                if (space[z][vector.y][vector.x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+            if (vector.y < 0 || vector.y > yLength + 1) {
                 return false;
             }
 
-            // Y up down
-            foundObstacle = false;
-            for (let y=vector.y + 1; y < yLength; y++) {
-                if (space[vector.z][y][vector.x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+            if (vector.z < 0 || vector.z > zLength + 1) {
                 return false;
             }
 
-            foundObstacle = false;
-            for (let y=vector.y - 1; y >= 0; y--) {
-                if (space[vector.z][y][vector.x]) {
-                    foundObstacle = true;
-                    break;
+            return inverseSpace[vector.z][vector.y][vector.x];
+        }
+
+        const adjacentVectors: Vector[] = [
+            {x: 0, y: 0, z: 1},
+            {x: 0, y: 0, z:-1},
+            {x: 0, y: 1, z: 0},
+            {x: 0, y:-1, z: 0},
+            {x: 1, y: 0, z: 0},
+            {x:-1, y: 0, z: 0}
+        ];
+        const rootCube: Vector = new Vector(0, 0, 0);
+        let discoveredCubes: Vector[] = [];
+        let suspect: Vector | undefined = rootCube;
+        while (suspect !== undefined) {
+            const newSuspects: Vector[] = adjacentVectors
+                // @ts-ignore
+                .map(direction => new Vector(suspect.x + direction.x, suspect.y + direction.y, suspect.z + direction.z))
+                .filter(target => isOccupiedInInverseSpace(target));
+            discoveredCubes = discoveredCubes.concat(newSuspects);
+            inverseSpace[suspect.z][suspect.y][suspect.x] = false;
+            suspect = discoveredCubes.pop();
+        }
+
+        for (let z=1; z < zLength + 1; z++) {
+            for (let y=1; y < yLength + 1; y++) {
+                for (let x=1; x < xLength + 1; x++) {
+                    if (inverseSpace[z][y][x]) {
+                        space[z - 1][y - 1][x - 1] = true;
+                    }
                 }
             }
-            if (!foundObstacle) {
+        }
+
+        const isOccupiedInNormalSpace = (vector: Vector) => {
+            if (vector.x - minX < 0 || vector.x - minX >= xLength) {
                 return false;
             }
 
-            foundObstacle = false;
-            for (let x=vector.x + 1; x < xLength; x++) {
-                if (space[vector.z][vector.y][x]) {
-                    foundObstacle = true;
-                    break;
-                }
-            }
-            if (!foundObstacle) {
+            if (vector.y - minY < 0 || vector.y - minY >= yLength) {
                 return false;
             }
 
-            foundObstacle = false;
-            for (let x=vector.x - 1; x >= 0; x--) {
-                if (space[vector.z][vector.y][x]) {
-                    foundObstacle = true;
-                    break;
-                }
+            if (vector.z - minZ < 0 || vector.z - minZ >= zLength) {
+                return false;
             }
 
-            return foundObstacle;
-        });
-
-        insideHoles.forEach(holeVector => space[holeVector.z][holeVector.y][holeVector.x] = true);
+            return space[vector.z - minZ][vector.y - minY][vector.x - minX];
+        }
 
         let exposedSurfaces: number = 0;
-        cubes.forEach(cube => {
-            if (!isOccupied(space, new Vector(cube.x, cube.y, cube.z + 1))) {
-                exposedSurfaces++;
-            }
-
-            if (!isOccupied(space, new Vector(cube.x, cube.y, cube.z - 1))) {
-                exposedSurfaces++;
-            }
-
-            if (!isOccupied(space, new Vector(cube.x, cube.y + 1, cube.z))) {
-                exposedSurfaces++;
-            }
-
-            if (!isOccupied(space, new Vector(cube.x, cube.y - 1, cube.z))) {
-                exposedSurfaces++;
-            }
-
-            if (!isOccupied(space, new Vector(cube.x + 1, cube.y, cube.z))) {
-                exposedSurfaces++;
-            }
-
-            if (!isOccupied(space, new Vector(cube.x - 1, cube.y, cube.z))) {
-                exposedSurfaces++;
-            }
-        });
+        cubes
+            .filter(cube => space[cube.z - minZ][cube.y - minY][cube.x - minX])
+            .forEach(cube => {
+                exposedSurfaces += adjacentVectors.filter(vector => !isOccupiedInNormalSpace(new Vector(cube.x + vector.x, cube.y + vector.y, cube.z + vector.z))).length;
+            });
 
         console.log("Total surface area: ", exposedSurfaces);
     }
